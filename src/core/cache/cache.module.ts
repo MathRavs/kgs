99e11e405e80c7ae6cfb4759730358886cbe11fa
MoonCypher <mathiasravelojaona@gmail.com>
2025-01-31 08:18:49 +0100

    feat(back): :sparkles: fix caching behavior

--- src/core/cache/cache.module.ts
+++ src/core/cache/cache.module.ts
@@ -1,22 +1,32 @@
 import { ConfigService } from '@nestjs/config';
 import { ConfigurationType } from '../configuration/configuration.type';
 import { CacheModule as CacheManagerModule } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-redis-store';
 import { CacheOptions } from '@nestjs/cache-manager/dist/interfaces/cache-module.interface';
+import KeyvRedis from '@keyv/redis';
+import Keyv from 'keyv';
 
 export const CacheModule = CacheManagerModule.registerAsync({
   inject: [ConfigService],
   isGlobal: true,
   useFactory: async (
     configService: ConfigService<ConfigurationType>,
-  ): Promise<CacheOptions> => ({
-    isGlobal: true,
-    store: await redisStore.redisStore({
-      socket: {
-        host: configService.get('REDIS_HOST'),
-        port: configService.get('REDIS_PORT'),
-      },
-      password: configService.get('REDIS_PASSWORD'),
-    }),
-  }),
+  ): Promise<CacheOptions> => {
+    const keyv = new Keyv({
+      store: new KeyvRedis(
+        `redis://:${configService.get('REDIS_PASSWORD')}@${configService.get('REDIS_HOST')}:${configService.get('REDIS_PORT')}`,
+      ),
+    });
+
+    keyv.on('error', (err) => {
+      console.error('Connection Error', err);
+      // eslint-disable-next-line unicorn/no-process-exit
+      process.exit(0);
+    });
+
+    return {
+      isGlobal: true,
+      stores: keyv,
+      ttl: 60_000,
+    };
+  },
 });
