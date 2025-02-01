e0221b9644d8583d44b0eb3064a4986f6811e16d
MoonCypher <mathiasravelojaona@gmail.com>
2025-02-01 07:03:07 +0100

    style(back): :lipstick: update app module typescript paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { validationSchema } from './core/configuration/configuration.type';
+import { validationSchema } from '@core/configuration/configuration.type';
 import CoreFeatures from './core/features/core.features';
-import { DatabaseModule } from './core/database/database.module';
-import { ConfigurationDataLoader } from './core/configuration/configuration';
-import { EncryptionModule } from './core/encryption/encryption.module';
-import { UrlShortenerModule } from './features/url-shortener/url-shortener.module';
-import { CacheModule } from './core/cache/cache.module';
+import { DatabaseModule } from '@core/database/database.module';
+import { ConfigurationDataLoader } from '@core/configuration/configuration';
+import { EncryptionModule } from '@core/encryption/encryption.module';
+import { UrlShortenerModule } from '@feature/url-shortener/url-shortener.module';
+import { CacheModule } from '@core/cache/cache.module';
 
 const features = [UrlShortenerModule];
 
