009140eaadad9cdfe46d07d90f448f48423a7bf3
MoonCypher <mathiasravelojaona@gmail.com>
2025-02-02 17:09:11 +0100

    fix(back/api-keys): add api key guard

--- src/core/features/api-keys/guards/api-key.guard.ts
+++ src/core/features/api-keys/guards/api-key.guard.ts
@@ -1,25 +1,42 @@
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   Injectable,
+  Logger,
   UnauthorizedException,
 } from '@nestjs/common';
 import { AbstractApiKeyService } from '../services/abstract/abstract-api-key.service';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  constructor(private readonly apiKeyService: AbstractApiKeyService) {}
+  @Inject()
+  private readonly apiKeyService: AbstractApiKeyService;
+
+  @Inject()
+  private readonly logger: Logger;
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    this.logger.debug('Checking api key', this.constructor.name);
+
     const request = context.switchToHttp().getRequest();
     const apiKey = request.headers['x-api-key'];
+
     if (!apiKey) {
-      throw new UnauthorizedException('the api key is invalid');
+      this.logger.error('No api key is provided', this.constructor.name);
+      throw new UnauthorizedException('the api key is mandatory');
     }
+
+    this.logger.debug('Retrieving api key', this.constructor.name);
+
     const key = await this.apiKeyService.findApiKey(apiKey);
+
     if (!key) {
+      this.logger.debug('Invalid api key', this.constructor.name);
       throw new UnauthorizedException('the api key is invalid');
     }
+    this.logger.debug('Api key retrieved', this.constructor.name);
+
     request.user = key.owner;
     return true;
   }
