d0c3e0bde5a45aa7d87a309b372e669e36205a78
MoonCypher <mathiasravelojaona@gmail.com>
2025-02-02 16:27:32 +0100

    feat(back): add logging to auth service

---
+++ src/core/features/auth/services/implementation/auth.service.ts
@@ -0,0 +1,41 @@
+import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AbstractUserRepository } from '../../../user/repositories/abstract/abstract-user.repository';
+import { BcryptService } from '@core/encryption/bcrypt.service';
+import { AbstractAuthService } from '../abstracts/abstract-auth.service';
+
+@Injectable()
+export class AuthService implements AbstractAuthService {
+  @Inject()
+  private readonly jwtService: JwtService;
+
+  @Inject()
+  private readonly userRepository: AbstractUserRepository;
+
+  @Inject()
+  private readonly bcryptService: BcryptService;
+
+  @Inject()
+  private readonly logger: Logger;
+
+  async login(email: string, password: string) {
+    this.logger.log(`Logging in user`, this.constructor.name);
+    const user = await this.userRepository.findUserByEmail(email);
+
+    if (!this.bcryptService.verifyPassword(password, user.password)) {
+      this.logger.warn(`Password check fails`, this.constructor.name);
+
+      throw new NotFoundException('User not found', {
+        cause: new Error('User not found'),
+        description: 'Invalid password',
+      });
+    }
+    const token = this.jwtService.sign({
+      email: user.email,
+      id: user.id,
+    });
+
+    this.logger.log(`Logging successful`, this.constructor.name);
+    return token;
+  }
+}
