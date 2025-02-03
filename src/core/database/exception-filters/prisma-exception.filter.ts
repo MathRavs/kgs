2fecd49f2338751a307fadd1b5aadc8a7d45b654
MoonCypher <mathiasravelojaona@gmail.com>
2025-02-03 07:57:42 +0100

    refactor(back/logging): add missing logging

--- src/core/database/exception-filters/prisma-exception.filter.ts
+++ src/core/database/exception-filters/prisma-exception.filter.ts
@@ -3,17 +3,28 @@ import {
   Catch,
   ExceptionFilter,
   HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
 } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { Response } from 'express';
 
+@Injectable()
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaExceptionFilter implements ExceptionFilter {
+  @Inject()
+  private readonly logger: Logger;
+
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
+    this.logger.debug(exception, this.constructor.name);
+
     if (exception.code === 'P2002') {
+      this.logger.warn('entity already exists', this.constructor.name);
+
       const target =
         (exception.meta?.target as string[])?.join(', ') || 'field';
       response.status(HttpStatus.CONFLICT).json({
@@ -22,12 +33,16 @@ export class PrismaExceptionFilter implements ExceptionFilter {
         error: 'Conflict',
       });
     } else if (exception.code === 'P2025') {
+      this.logger.warn('entity not found', this.constructor.name);
+
       response.status(HttpStatus.NOT_FOUND).json({
         statusCode: HttpStatus.NOT_FOUND,
         message: `Entity not found`,
         error: 'Not found',
       });
     } else {
+      this.logger.error(exception, this.constructor.name);
+
       // Handle other Prisma errors if necessary
       response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
         statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
--- src/core/database/repositories/implementation/prisma-sequence-manager.repository.ts
+++ src/core/database/repositories/implementation/prisma-sequence-manager.repository.ts
@@ -1,13 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { AbstractSequenceManagerRepository } from '../abstract/abstract-sequence-manager.repository';
 import { SequencesEnum } from '../../enums/sequences.enum';
 import { PrismaService } from '../../prisma.service';
 
 @Injectable()
 export class PrismaSequenceManagerRepository extends AbstractSequenceManagerRepository {
-  constructor(private readonly prismaService: PrismaService) {
-    super();
-  }
+  @Inject()
+  private readonly prismaService: PrismaService;
 
   async getNextVal(sequenceName: SequencesEnum): Promise<{ id: string }> {
     const result = await this.prismaService
